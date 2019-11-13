28d6ebc6ca31cb868a0a0d9d73cd5f8603585695
Oli Wright <github@falo.co.uk>
2019-11-13 21:31:08 +0000

    Slowed down Z-Probe Speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1327,7 +1327,7 @@
 
 // Homing speeds (mm/m)
 #define HOMING_FEEDRATE_XY (30*60)
-#define HOMING_FEEDRATE_Z  (20*60)
+#define HOMING_FEEDRATE_Z  (10*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
