1be190a7f938beec08f87c0eae86f90377f2267c
Oli Wright <github@falo.co.uk>
2019-11-17 12:28:34 +0000

    Probe offsets for petsfang

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -924,7 +924,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -4, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { +48, -2, 0 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
