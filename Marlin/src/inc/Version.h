6bb55cb063eddeb84068a5c9cc132ac5732ab172
Oli Wright <github@falo.co.uk>
2019-12-12 15:44:54 +0000

    Fixed broken Version.h

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -42,11 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-<<<<<<< HEAD
-  #define STRING_DISTRIBUTION_DATE "2019-12-01"
-=======
   #define STRING_DISTRIBUTION_DATE "2019-12-04"
->>>>>>> remotes/origin/bugfix-2.0.x
 #endif
 
 /**
