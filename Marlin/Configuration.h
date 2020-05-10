1e1bb2f8beb5a9a4d238cdea15bafe2a4ca472c9
Oli Wright <github@falo.co.uk>
2020-05-10 13:38:21 +0100

    Y bed limit for new plate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1116,11 +1116,11 @@
 
 // The size of the print bed
 #define X_BED_SIZE 230
-#define Y_BED_SIZE 230
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -20
-#define Y_MIN_POS 0
+#define Y_MIN_POS -16
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
