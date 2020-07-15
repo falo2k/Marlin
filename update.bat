a95fb565c0272d26f066e1be8742eba50c426615
Oli Wright <github@falo.co.uk>
2020-07-15 12:53:41 +0100

    update script added build

--- update.bat
+++ update.bat
@@ -4,4 +4,4 @@ git merge remotes/upstream/bugfix-2.0.x
 git push origin HEAD:bugfix-2.0.x
 git checkout bufgix-2.0.x_SKRminiE3
 git merge remotes/origin/bugfix-2.0.x
-git push
+git push && platformio build && explorer .pio\build\STM32F103RC_btt_512K\
