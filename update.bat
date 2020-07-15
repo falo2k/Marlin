ad689a1cabd3bfc1011ae3cd137d02a1cea029e1
Oli Wright <github@falo.co.uk>
2020-07-15 12:38:21 +0100

    Update script and changed default build

---
+++ update.bat
@@ -0,0 +1,7 @@
+git fetch upstream
+git checkout remotes/origin/bugfix-2.0.x
+git merge remotes/upstream/bugfix-2.0.x
+git push origin HEAD:bugfix-2.0.x
+git checkout bufgix-2.0.x_SKRminiE3
+git merge remotes/origin/bugfix-2.0.x
+git push
