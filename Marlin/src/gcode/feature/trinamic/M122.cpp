b1dabfa91f7f5d6f14001d6a3fe9299031d3a591
Oli Wright <1392563+falo2k@users.noreply.github.com>
2021-05-12 10:15:22 +0100

    Revert hacky fix to official fix

--- Marlin/src/gcode/feature/trinamic/M122.cpp
+++ Marlin/src/gcode/feature/trinamic/M122.cpp
@@ -28,13 +28,11 @@
 #include "../../../feature/tmc_util.h"
 #include "../../../module/stepper/indirection.h"
 
-#define NUM_AXIS 4
-
 /**
  * M122: Debug TMC drivers
  */
 void GcodeSuite::M122() {
-  xyze_bool_t print_axis = ARRAY_N_1(NUM_AXIS, false);
+  xyze_bool_t print_axis = ARRAY_N_1(XYZE, false);
 
   bool print_all = true;
   LOOP_XYZE(i) if (parser.seen(axis_codes[i])) { print_axis[i] = true; print_all = false; }
